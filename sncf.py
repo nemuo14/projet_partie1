c6dbe50abf8d5688d97d48b165ee239987d74b8e
zelmano <zelman.brun@gmail.com>
2024-06-05 11:29:57 +0200

    joli dessin d1

--- sncf.py
+++ sncf.py
@@ -2,6 +2,24 @@
 # nom longueur largeur hauteur
 import csv
 
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Dessin de plusieurs rectangles")
+
+# Création du canvas
+canvas = tk.Canvas(root, width=1600, height=600)
+canvas.pack()
+root.state('zoomed')
+
+def dessiner_quoi(canvas, x, y, longueur, largeur, couleur="brown", texte=""):
+    canvas.create_rectangle(x, y, x + largeur, y + longueur, outline="black", fill=couleur)
+    canvas.create_text(x + largeur/2, y + longueur / 2, text=texte, fill="white")
+
+def dessiner_marchandise(canvas, x, y, longueur, largeur):
+    canvas.create_rectangle(x, y, x + longueur, y + largeur, outline="black", fill="green")
+
+
 data_marchandises = []
 # all_marchandises=[]
 with open("Données marchandises.csv", mode='r', newline='') as csvmarchandises:
@@ -21,30 +39,69 @@ with open("Données marchandises.csv", mode='r', newline='') as csvmarchandises:
 
         data_marchandises.append(row)
 
-    print(data_marchandises)
+    #print(data_marchandises)
 
 # longeur largeur hauteur
 dimensions_wagon = [11.583, 2.294, 2.569]
-
-train = [[]]
 
 
 def longeur_dispo_wagon(wagon):
     return dimensions_wagon[0] - sum([marchandise[1] for marchandise in wagon])
 
+def charger_train(data_marchandises, train):
+    while len(data_marchandises) > 0:
+        liste_place_dispo = []
+        for i in range(len(train)):
+            wagon = train[i]
+            if longeur_dispo_wagon(wagon) >= data_marchandises[0][1]:
+                liste_place_dispo.append([i,longeur_dispo_wagon(wagon)])
 
-while len(data_marchandises) > 0:
-    # on parcourt tout le train par wagons.
-    # on calcule la place disponible dans chaque wagon et on stocke le résultat dans une liste.
-    # on met dans cette liste uniquement les wagons qui ont la place suffisante pour la marchandise.
-    # on met ensuite la marchandise dans le wagon qui a le moins de place disponible (min de la liste).
-    # si aucun wagon n'a la place suffisante, on rajoute un wagon et on met la marchandise dedans.
+        place_min = 1000000
+        index_place_min = 0
+        for j in range(len(liste_place_dispo)):
+            if liste_place_dispo[j][1] < place_min:
+                place_min = liste_place_dispo[j][1]
+                index_place_min = liste_place_dispo[j][0]
+        
+        if len(liste_place_dispo) == 0:
+            train.append([data_marchandises.pop(0)])
+        else:
+            train[index_place_min].append(data_marchandises.pop(0))
+    return train
 
-    liste_place_dispo = []
-    for wagon in train:
-        print(wagon)
-        print(longeur_dispo_wagon(wagon))
-    pass
+def dessine_train_d1(train):
+    window_width = 1400
+
+    size_multiplier = 12
+    small_margin = 3
+    wagon_width = 100
+
+    wagon_coords = [10,10]
+    march_coords = [10,10]
+
+    for i in range(len(train)):
+        wagon = train[i]
+        dessiner_quoi(canvas, wagon_coords[0], wagon_coords[1], dimensions_wagon[0]*size_multiplier, wagon_width, "brown")
+        for j in range(len(train[i])):
+            marchandise = train[i][j]
+            dessiner_quoi(canvas, march_coords[0], march_coords[1], marchandise[1]*size_multiplier, wagon_width, "green", marchandise[0])
+            march_coords[1] += marchandise[1]*size_multiplier
+
+        march_coords[0] += 110
+        march_coords[1] = wagon_coords[1]
+        wagon_coords[0] += 110
+
+        if wagon_coords[0] > window_width:
+            wagon_coords[0] = 10
+            wagon_coords[1] += 150
+            march_coords[0] = 10
+            march_coords[1] += 150
+    
+    canvas.create_text(1200,500, text="nombre de wagons : "+str(len(train)), fill="black")
+
+    root.mainloop()
+
+
 
 # repush
 def tri_d1(data_marchandises):
@@ -60,12 +117,21 @@ def tri_d2(data_marchandises):
 def tri_d3(data_marchandises):
     for i in range(len(data_marchandises)):
         data_marchandises[i].append(data_marchandises[i][1] * data_marchandises[i][2] * data_marchandises[i][3])
-    return sorted(data_marchandises, key=lambda x: x[5], reverse=True)
+    return sorted(data_marchandises, key=lambda x: x[5], reverse=True)#attention à combien on utilise de bananes dans la vallée ohoh de dana lalilala
 
 
 data_marchandises_offline_d1 = tri_d1(data_marchandises)
-print(data_marchandises_offline_d1)
+#print(data_marchandises_offline_d1)
 data_marchandises_offline_d2 = tri_d2(data_marchandises)
-print(data_marchandises_offline_d2)
+#print(data_marchandises_offline_d2)
 data_marchandises_offline_d3 = tri_d3(data_marchandises)
-print(data_marchandises_offline_d3)
+#print(data_marchandises_offline_d3)
+
+train_patrie = [[]]
+train_patrie = charger_train(data_marchandises, train_patrie)
+dessine_train_d1(train_patrie)
+
+# train1 = [[]]
+# train1 = charger_train(data_marchandises_offline_d1, train1)
+# #print(train1)
+# dessine_train_d1(train1)
