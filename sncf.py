92a4465b06e65385fad868fe8febb8f211676a69
zelmano <zelman.brun@gmail.com>
2024-06-05 09:03:18 +0200

    formattage

--- sncf.py
+++ sncf.py
@@ -1,13 +1,27 @@
-#galvanised square steele
+#galvanised square steel
+#nom longueur largeur hauteur
 import csv
-data_marchandises={}
-all_marchandises=[]
+data_marchandises=[]
+#all_marchandises=[]
 with open("Données marchandises.csv",mode='r',newline='') as csvmarchandises:
     marchandises=csv.reader(csvmarchandises,delimiter=';')
-    for elements in marchandises:
-        data_marchandises.append(
+    for row in marchandises:
+    
+        if row[0] == 'Numéro ':#skip la première ligne
+            continue
 
-        print(row)
+        row.pop(0)#on enlève le numéro
+
+        row[0] = row[0].replace(' ','')#on enlève les espaces
+        
+        row[1] = float(row[1].replace(',','.'))#on remplace les virgules par des points
+        row[2] = float(row[2].replace(',','.'))
+        row[3] = float(row[3].replace(',','.'))
+
+        
+        #data_marchandises.append(
+
+        data_marchandises.append(row)
 
         #data_marchandises.append(dict(row))
     print(data_marchandises)
