1cf91f543be9e8d7c4757858373dc92e3fd94e4d
zelmano <zelman.brun@gmail.com>
2024-06-05 14:03:37 +0200

    poutch

--- sncf.py
+++ sncf.py
@@ -16,8 +16,7 @@ def dessiner_quoi(canvas, x, y, longueur, largeur, couleur="brown", texte=""):
     canvas.create_rectangle(x, y, x + largeur, y + longueur, outline="black", fill=couleur)
     canvas.create_text(x + largeur/2, y + longueur / 2, text=texte, fill="white")
 
-def dessiner_marchandise(canvas, x, y, longueur, largeur):
-    canvas.create_rectangle(x, y, x + longueur, y + largeur, outline="black", fill="green")
+
 
 """
       _               _
@@ -62,9 +61,46 @@ def dessine_train_d1(train):
 
 
 
+"""
+            _            _
+   ___ __ _| | ___ _   _| |___
+  / __/ _` | |/ __| | | | / __|
+ | (_| (_| | | (__| |_| | \__ \
+  \___\__,_|_|\___|\__,_|_|___/
 
+"""
 
+def chercher_index_place_min(liste_place_dispo):
+    #liste_place_dispo : [[index_wagon, place_dispo], ...]
+    #retourne l'index du wagon avec le moins de place dispo dans la liste
+    #on parcourt toute la liste, si on trouve un meilleur minimum, on le garde et on note son index
+    place_min = 1000000
+    index_place_min = 0
+    for i in range(len(liste_place_dispo)):
+        if liste_place_dispo[i][1] < place_min:
+            place_min = liste_place_dispo[i][1]
+            index_place_min = liste_place_dispo[i][0]
+    return index_place_min
 
+def charger_train_d1(data_marchandises, train):
+    #data_marchandises (la liste de toutes les marchandises à rentrer): [[nom, longueur, largeur, hauteur], ...]
+    #train (la liste des wagons): [[marchandise1, marchandise2, ...], ...]
+    #retourne le train avec tous les wagons remplis avec toutes les marchandises
+    #on parcourt toutes les marchandises, 
+    while len(data_marchandises) > 0:
+        liste_place_dispo = []
+        for i in range(len(train)):
+            wagon = train[i]
+            if longeur_dispo_wagon(wagon) >= data_marchandises[0][1]:
+                liste_place_dispo.append([i,longeur_dispo_wagon(wagon)])
+
+        index_place_min = chercher_index_place_min(liste_place_dispo)
+        
+        if len(liste_place_dispo) == 0:
+            train.append([data_marchandises.pop(0)])
+        else:
+            train[index_place_min].append(data_marchandises.pop(0))
+    return train
 
 
 
@@ -114,27 +150,6 @@ dimensions_wagon = [11.583, 2.294, 2.569]
 def longeur_dispo_wagon(wagon):
     return dimensions_wagon[0] - sum([marchandise[1] for marchandise in wagon])
 
-def charger_train(data_marchandises, train):
-    while len(data_marchandises) > 0:
-        liste_place_dispo = []
-        for i in range(len(train)):
-            wagon = train[i]
-            if longeur_dispo_wagon(wagon) >= data_marchandises[0][1]:
-                liste_place_dispo.append([i,longeur_dispo_wagon(wagon)])
-
-        place_min = 1000000
-        index_place_min = 0
-        for j in range(len(liste_place_dispo)):
-            if liste_place_dispo[j][1] < place_min:
-                place_min = liste_place_dispo[j][1]
-                index_place_min = liste_place_dispo[j][0]
-        
-        if len(liste_place_dispo) == 0:
-            train.append([data_marchandises.pop(0)])
-        else:
-            train[index_place_min].append(data_marchandises.pop(0))
-    return train
-
 
 
 # repush
@@ -162,7 +177,7 @@ data_marchandises_offline_d3 = tri_d3(data_marchandises)
 #print(data_marchandises_offline_d3)
 
 train_patrie = [[]]
-train_patrie = charger_train(data_marchandises, train_patrie)
+train_patrie = charger_train_d1(data_marchandises, train_patrie)
 dessine_train_d1(train_patrie)
 
 # train1 = [[]]
