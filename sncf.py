e15614d712590a5d7e71d486419ecdae3cd8fea2
zelmano <zelman.brun@gmail.com>
2024-06-05 09:20:47 +0200

    I commit

--- sncf.py
+++ sncf.py
@@ -18,12 +18,28 @@ with open("Données marchandises.csv",mode='r',newline='') as csvmarchandises:
         row[2] = float(row[2].replace(',','.'))
         row[3] = float(row[3].replace(',','.'))
 
-        
-        #data_marchandises.append(
-
         data_marchandises.append(row)
 
-        #data_marchandises.append(dict(row))
     print(data_marchandises)
-    # for i in range(len(data_marchandises)):
-    #     all_marchandises.append(data_marchandises[i][data_marchandises[i].values()])
+
+#longeur largeur hauteur
+dimensions_wagon = [11.583,2.294,2.569]
+
+train = [[]]
+
+def longeur_dispo_wagon(wagon):
+    return dimensions_wagon[0] - sum([marchandise[1] for marchandise in wagon])
+
+while len(data_marchandises) > 0:
+    #on parcourt tout le train par wagons.
+    #on calcule la place disponible dans chaque wagon et on stocke le résultat dans une liste.
+    #on met dans cette liste uniquement les wagons qui ont la place suffisante pour la marchandise.
+    #on met ensuite la marchandise dans le wagon qui a le moins de place disponible (min de la liste).
+    #si aucun wagon n'a la place suffisante, on rajoute un wagon et on met la marchandise dedans.
+    
+    liste_place_dispo = []
+    for wagon in train:
+        print(wagon)
+        print(longeur_dispo_wagon(wagon))
+    pass
+    
